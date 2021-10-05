7f2c5919a4dc748abfe504529889566c19aeed3e
Silas-assis <silaslima-19@hotmail.com>
2021-10-05 16:30:53 -0300

    BufferedReader e Classe Cliente

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,10 +6,60 @@
  */
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Cliente;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		//Caminho do arquivo csv da atividade.
+		String path = "C:\\Users\\LEDcl\\eclipse-workspace\\pota-21270887\\arquivoDados.csv";  
+		
+		//Lista de Clientes para armazenar informações de cada cliente.
+		List<Cliente> list = new ArrayList<Cliente>();
+		
+		try(BufferedReader leitor = new BufferedReader(new FileReader(path))){
+			
+			String linha = leitor.readLine();
+			
+			/*
+			 * Enquanto o while for diferente de null, o algoritmo vai ler a próxima linha. Utilizando o 
+			 * método Split, que recortar com base na virgula e guardando cada informação em um vetor auxiliar.
+			 * Após percorrer a linha inteira, irá instanciar o cliente e em seguida adicionar ao ArrayList.
+			 * No final, o algoritmo lê a próxima linha e repete o processo até a próxima linha ser vazia. 
+			 */
+			while(linha != null) {
+				
+				String [] vetor = linha.split(",");
+				String nome = vetor[0];
+				String sexo = vetor[1];
+				String endereco = vetor[2];
+				String cidade = vetor[3];
+				String email = vetor[4];
+				String telefone = vetor[5];
+				String idade = vetor[6];
+				
+				Cliente cliente = new Cliente(nome, sexo, endereco, cidade, email, telefone, idade);
+				list.add(cliente);
+				
+				linha = leitor.readLine(); // Ler a próxima linha
+			}
+				
+		}catch(IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		
+		System.out.println("O arquivo contém: " + list.size() + " registros de clientes.");
+		
+		/*System.out.println("Lista de Pessoas: ");
+		for (Cliente c : list) {
+			System.out.println(c);
+		}*/
 	}
-
 }
---
+++ src/entities/Cliente.java
@@ -0,0 +1,89 @@
+package entities;
+
+public class Cliente {
+
+	private String nome;
+	private String sexo;
+	private String endereco;
+	private String cidade;
+	private String email;
+	private String telefone;
+	private String idade;
+
+	public Cliente() {
+	}
+
+	public Cliente(String nome, String sexo, String endereco, String cidade, String email, String telefone,
+			String idade) {
+		this.nome = nome;
+		this.sexo = sexo;
+		this.endereco = endereco;
+		this.cidade = cidade;
+		this.email = email;
+		this.telefone = telefone;
+		this.idade = idade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getIdade() {
+		return idade;
+	}
+
+	public void setIdade(String idade) {
+		this.idade = idade;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente: " + nome + ", " + sexo + ", " + endereco + ", " + cidade + ", " + email + ", " + telefone + ", "
+				+ idade;
+	}
+
+}
