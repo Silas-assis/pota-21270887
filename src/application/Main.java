c7461d64fdb6eef08b82edf153434996e30d4852
Silas-assis <silaslima-19@hotmail.com>
2021-10-05 13:55:22 -0300

    Projeto Criado
    Criação da Classe Main, responsável pela execução e Classe
    BuscaBinaria, responsável pela busca no arquivo.

---
+++ src/application/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Atividade 1 - POTA
+ * Grupo: Silas Lima de Assis - RA: 21270887, Gustavo Volpe Santos - RA: 21363230, 
+ * Lucas Garcia M. Zampirolli - RA: 21238938, Murilo Zinezi - RA: 21272578,
+ * Reinaldo Muraro Vieira Filho - RA: 21099060
+ */
+package application;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
